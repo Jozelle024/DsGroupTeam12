c2b55c5d5a86f891a1ea62b59f5b397180d8c489
Jozelle <jozellelauzon024@gmail.com>
2018-03-05 17:09:18 +0100

    creazione branch develop,installazione librerie, gulp task

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/* jshint esversion: 6 */
+
+// jshint ignore: start
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+
+gulp.task('default',function(){
+    return gutil.log('Gulp in esecuzione');
+});
+
+gulp.task('copiaFile', function(){
+    gulp.src('source/html/*.html').pipe(gulp.dest('dist'));
+    gulp.src('source/javascript/*.js').pipe(gulp.dest('dist'));
+    gulp.src('source/css/*.css').pipe(gulp.dest('dist'));
+});
+
+// to configure jshint task
+
+gulp.task('jshint', function() {
+    return gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('sass', function(){
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sassWatch',function(){
+    gulp.watch('./sass/*.scss', ['sass']);
+});
