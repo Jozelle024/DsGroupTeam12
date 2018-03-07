2ec3c526c6961561e53aad8d3df1f24d21a3fe4a
Amila Fernando <amila_fernandp92@gmail.com>
2018-03-07 12:13:08 +0100

    modifiche

--- source/javascript/function.js
+++ source/javascript/function.js
@@ -1,36 +1,8 @@
 $(document).ready (function () {
-    <div class="rowcontainer clearfix">
-        <div class="colcontainer">
-            <a href="#">
-                <div class="imgcontainer img"></div>
-                <div class="colcontainerfade">
-                    <div class="titlecontainer">
-                        <h1>Se gli astronomi usano l’intelligenza artificiale per scovare le lenti gravitazionali</h1>
-                        <h2>Un algoritmo nutrito con milioni di immagini di una porzione di cielo individua centinaia di lenti, poi ridotte a 56 dagli astronomi, essenziali per indagare la materia oscura</h2>
-                        <span>continua...</span>
-                    </div>
-                    <div class="footercontainer">
-                        <h4 class="section">Inteligenza Artificiale - Storie</h4>
-                        <h4 class="author">Simone Cosimi</h4>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="colcontainer">
-            <a href="#">
-                <div class="imgcontainer img"></div>
-                <div class="colcontainerfade">
-                    <div class="titlecontainer">
-                        <h1>Se gli astronomi usano l’intelligenza artificiale per scovare le lenti gravitazionali</h1>
-                        <h2>Un algoritmo nutrito con milioni di immagini di una porzione di cielo individua centinaia di lenti, poi ridotte a 56 dagli astronomi, essenziali per indagare la materia oscura</h2>
-                        <span>continua...</span>
-                    </div>
-                    <div class="footercontainer">
-                        <h4 class="section">Inteligenza Artificiale - Storie</h4>
-                        <h4 class="author">Simone Cosimi</h4>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-})
+    var app = require ("../json/news.json");
+        console.log(app.news.headsection);
+        /*$("h1").append(data.news.maintitle);
+        $("h2").append(data.news.subtitle);
+        $("h4.section").append(data.news.headsection + "-" +data.news.subsection);
+        $("h4.author").append(data.news.author.name);*/
+});
