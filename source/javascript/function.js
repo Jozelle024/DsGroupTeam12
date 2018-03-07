63e889d0b1196e433520603ed40fd5d8ac833478
Amila Fernando <amila_fernandp92@gmail.com>
2018-03-07 12:37:49 +0100

    modifiche

--- source/javascript/function.js
+++ source/javascript/function.js
@@ -1,8 +1,10 @@
-$(document).ready (function () {
-    var app = require ("../json/news.json");
-        console.log(app.news.headsection);
-        /*$("h1").append(data.news.maintitle);
-        $("h2").append(data.news.subtitle);
-        $("h4.section").append(data.news.headsection + "-" +data.news.subsection);
-        $("h4.author").append(data.news.author.name);*/
-});
+var app = require ("news.js");
+
+function parti () {
+    console.log(app.getId ());
+    
+    /*$("h1").append(data.news.maintitle);
+    $("h2").append(data.news.subtitle);
+    $("h4.section").append(data.news.headsection + "-" +data.news.subsection);
+    $("h4.author").append(data.news.author.name);*/
+}
