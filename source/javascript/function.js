5ede42603f942e4c93c2f18675a51500eba4eaa6
Amila Fernando <amila_fernandp92@gmail.com>
2018-03-07 11:32:34 +0100

    modifiche

---
+++ source/javascript/function.js
@@ -0,0 +1,36 @@
+$(document).ready (function () {
+    <div class="rowcontainer clearfix">
+        <div class="colcontainer">
+            <a href="#">
+                <div class="imgcontainer img"></div>
+                <div class="colcontainerfade">
+                    <div class="titlecontainer">
+                        <h1>Se gli astronomi usano l’intelligenza artificiale per scovare le lenti gravitazionali</h1>
+                        <h2>Un algoritmo nutrito con milioni di immagini di una porzione di cielo individua centinaia di lenti, poi ridotte a 56 dagli astronomi, essenziali per indagare la materia oscura</h2>
+                        <span>continua...</span>
+                    </div>
+                    <div class="footercontainer">
+                        <h4 class="section">Inteligenza Artificiale - Storie</h4>
+                        <h4 class="author">Simone Cosimi</h4>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="colcontainer">
+            <a href="#">
+                <div class="imgcontainer img"></div>
+                <div class="colcontainerfade">
+                    <div class="titlecontainer">
+                        <h1>Se gli astronomi usano l’intelligenza artificiale per scovare le lenti gravitazionali</h1>
+                        <h2>Un algoritmo nutrito con milioni di immagini di una porzione di cielo individua centinaia di lenti, poi ridotte a 56 dagli astronomi, essenziali per indagare la materia oscura</h2>
+                        <span>continua...</span>
+                    </div>
+                    <div class="footercontainer">
+                        <h4 class="section">Inteligenza Artificiale - Storie</h4>
+                        <h4 class="author">Simone Cosimi</h4>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+})
